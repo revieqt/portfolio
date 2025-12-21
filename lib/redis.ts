e930dbeb58a67f7c38c4d4612969e0d8e96088e3
revieqt <opsima.josh@gmail.com>
2025-12-21 09:37:12 +0800

    debug nanaman

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: "https://settled-spaniel-40553.upstash.io",
+  token: "AZ5pAAIncDExNzE0NjNkMmZiYTc0ZGM0YjljM2I5NTU2ZTBhYjcyMnAxNDA1NTM",
 });
