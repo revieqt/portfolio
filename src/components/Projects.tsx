69f99d1c257212673ca3b26ac4d023c3da4f52aa
revieqt <opsima.josh@gmail.com>
2026-01-30 11:57:55 +0800

    Update: moved projects images to public

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
       title: "TaraG Travel App",
       description: "TaraG is a smart travel companion app that helps users plan trips, navigate routes, track locations, check weather, coordinate group travel, and access emergency assistance—all in one seamless experience.",
       tags: ["React Native", "React", "Typescript", "Node.js", "MongoDB"],
-      image: "src/assets/images/projects/tarag.jfif",
+      image: "/projects/tarag.jfif",
       link: "https://github.com",
       demo: "https://example.com"
     },
@@ -22,7 +22,7 @@ export default function Projects() {
       title: "Opswift POS",
       description: "a POS system with built-in MIS and inventory management, designed to streamline sales, track stock in real time, generate insightful reports, and support better business decisions from one unified platform.",
       tags: ["Java", "XAAMP", "MySQL"],
-      image: "src/assets/images/projects/opswift.jpg",
+      image: "/projects/opswift.jpg",
       link: "https://github.com",
       demo: "https://example.com"
     },
@@ -30,7 +30,7 @@ export default function Projects() {
       title: "NoteIt",
       description: "NoteIt is a simple and efficient note-taking app that helps users quickly capture ideas, organize notes, and stay productive anytime, anywhere.",
       tags: ["PHP", "Javascript", "HTML", "CSS"],
-      image: "src/assets/images/projects/noteit.PNG",
+      image: "/projects/noteit.PNG",
       link: "https://github.com",
       demo: "https://example.com"
     },
@@ -38,7 +38,7 @@ export default function Projects() {
       title: "Weather Forecast App",
       description: "Mobile-responsive weather application with location-based forecasts, hourly updates, and beautiful animations.",
       tags: ["React", "OpenWeather API", "Tailwind", "Geolocation"],
-      image: "src/assets/images/about_img.png",
+      image: "/icon.png",
       link: "https://github.com",
       demo: "https://example.com"
     },
